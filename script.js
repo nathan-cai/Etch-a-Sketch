a8cf40ce145fce8529baec0c9fd9cdb4ab81448f
Qien Nathan Cai <qien.nathan@gmail.com>
2022-08-16 09:43:39 -0700

    Add Slider functionality

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const middle = document.querySelector('#middle');
 const clear = document.querySelector('#clear');
+const currSize = document.querySelector('#currSize');
+const slider = document.querySelector('.slider');
 
 function createGrid(dimensions) {
     const size = 600/dimensions;
     for (let i = 0; i < dimensions * dimensions; i++) {
         const gridBox = document.createElement('div');
         gridBox.setAttribute('class', 'gridBox');
-        gridBox.setAttribute("style", `background-color: white; width: ${size}px; height: ${size}px; border: 0.2px solid lightgrey; box-sizing: border-box;`);
+        gridBox.setAttribute("style", `background-color: white; width: ${size}px; height: ${size}px; border: 0.04px solid #F0F0F0; box-sizing: border-box;`);
         gridBox.setAttribute('id', `a${i}`)
         gridBox.setAttribute('onmouseover', `draw(${i})`)
         middle.appendChild(gridBox);
@@ -28,4 +30,10 @@ function whiten() {
     const grid = document.querySelectorAll('.gridBox');
     grid.forEach(makeWhite);
 }
-createGrid(50); 
+createGrid(10); 
+
+slider.oninput = function () {
+    currSize.innerHTML = `${this.value} x ${this.value}`;
+    middle.textContent = '';
+    createGrid(this.value);
+}
